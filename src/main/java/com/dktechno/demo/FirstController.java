3b345d845fb05f0aadca31bcf5fd162f112441b7
matthias@mycumulus.com <matthias@mycumulus.com>
2025-01-12 17:53:06 +0000

    feat: cleanup OrderRecord record

--- src/main/java/com/dktechno/demo/FirstController.java
+++ src/main/java/com/dktechno/demo/FirstController.java
@@ -15,28 +15,4 @@ public class FirstController {
     public String post(@RequestBody String message) {
         return "request accepted and message is : " + message;
     }
-
-    @PostMapping("post-order")
-    public String post(@RequestBody Order order) {
-        return "Request accepted and order is : " + order.toString();
-    }
-
-
-    @PostMapping("post-order-record")
-    public String postOrderRecord(@RequestBody OrderRecord order) {
-        return "Request accepted and order is : " + order.toString();
-    }
-
-    @GetMapping("hello/{user-name}")
-    public String pathVar(@PathVariable("user-name") String username){
-        return "my value = "+ username;
-    }
-
-    // http://localhost:8080/hello2?param_name=paramvalu&param_value2=value2
-    @GetMapping("hello")
-    public String paramVar(@RequestParam("user-name") String username,
-                           @RequestParam("user-lastname") String userLastname){
-        return "my value = "+ username + "  " + userLastname;
-    }
-
 }
